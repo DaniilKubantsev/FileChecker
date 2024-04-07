603a97c2641bdb1e375fb74792a4066a572701cc
DaniilKubantsev <daniil.kubantsev@gmail.com>
2024-04-07 22:17:33 +0300

    is_pdf?() implementation

--- lib/FileChecker.rb
+++ lib/FileChecker.rb
@@ -7,14 +7,31 @@ module FileChecker
   class Error < StandardError; end
 
   class FileChecker
-    def is_png?()
-      #TODO
+
+    # Daniil Kubantsev
+    def is_png?(file_path)
+      # TODO: понять, как сопоставить расширение и содержимое
+      # begin
+      #   extencion = File.extname(path).delete('.').upcase
+      #   first_line = File.open(path, "r").readline
+      #   first_line == extencion
+      # rescue Errno::ENOENT => e
+      #   e.message
+      # end
     end
 
-    def is_pdf?()
-      #TODO
+    # Daniil Kubantsev
+    def is_pdf?(file_path)
+      begin
+        extencion = File.extname(path).delete('.').upcase
+        first_line = File.open(path, "r").readline.slice(1, 3)
+        first_line == extencion
+      rescue Errno::ENOENT => e
+        e.message
+      end
     end
-
+    
+    # Leonid Krupnov
     def is_json?(file_path)
       begin
         JSON.parse(File.read(file_path))
