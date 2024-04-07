b64122becaa78daf9597d40dd819e445794cfd7f
leopu55yhack <leonidkr700@icloud.com>
2024-04-07 21:08:43 +0300

    Added method is_json?()
    Added method is_json?()

--- lib/FileChecker.rb
+++ lib/FileChecker.rb
@@ -1,10 +1,11 @@
 # frozen_string_literal: true
+require 'json'
 
 require_relative "FileChecker/version"
 
 module FileChecker
   class Error < StandardError; end
-  
+
   class FileChecker
     def is_png?()
       #TODO
@@ -13,6 +14,16 @@ module FileChecker
     def is_pdf?()
       #TODO
     end
+
+    def is_json?(file_path)
+      begin
+        JSON.parse(File.read(file_path))
+        true
+      rescue JSON::ParserError => e
+        false
+      end
+    end
+
   end
 
 end
