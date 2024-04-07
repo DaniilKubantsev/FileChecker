adc75798bab714d8b8bcfd3b126583a8b8f38b36
DaniilKubantsev <daniil.kubantsev@gmail.com>
2024-04-07 19:27:54 +0300

    Create FileChecker
    asdasd

---
+++ lib/FileChecker.rb
@@ -0,0 +1,8 @@
+# frozen_string_literal: true
+
+require_relative "FileChecker/version"
+
+module FileChecker
+  class Error < StandardError; end
+  # Your code goes here...
+end
---
+++ lib/FileChecker/version.rb
@@ -0,0 +1,5 @@
+# frozen_string_literal: true
+
+module FileChecker
+  VERSION = "0.1.0"
+end
